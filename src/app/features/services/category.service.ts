9366e18eb7ab707ce08310182f6057be872eb650
ramlohith <ramlohith@MSI>
2024-02-23 19:05:55 +0530

    creaye environment files

--- src/app/features/services/category.service.ts
+++ src/app/features/services/category.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddcategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,11 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   addCategory(model: AddcategoryRequest): Observable<any> {
-    return this.http.post<any>('https://localhost:7296/api/Categories/', model);
+    return this.http.post<any>(`${environment.apiBaseUrl}/api/Categories/`, model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 
 
