6597aa1ea1a09014926cf8c83485b192ddf60fcf
ramlohith <ramlohith@MSI>
2024-03-28 20:49:44 +0530

    Fixed Update and Delete logic and screening - UI

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -5,6 +5,7 @@ import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../models/category.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -19,6 +20,10 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
   selectedCategories?: string[];
 
   routeSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
+  getBlogPostSusbscription?: Subscription;
+  deleteBlogPostSusbscription?: Subscription;
+
   constructor(private route: ActivatedRoute,
               private blogPostService: BlogPostService,
               private categoryService: CategoryService,
@@ -36,7 +41,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
         // Get BlogPost From API
         if(this.id)
         {
-          this.blogPostService.getBlogPostById(this.id).subscribe({
+          this.getBlogPostSusbscription = this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (resposne) => {
               this.model = resposne;
               this.selectedCategories = resposne.categories.map(x => x.id);
@@ -50,12 +55,44 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
     onFormSubmit(): void {
       if(this.model && this.id)
       {
-        console.log(this.model)
+        var updateBlogPost: UpdateBlogPost = {
+          author: this.model.author,
+          content: this.model.content,
+          shortDescription: this.model.shortDescription,
+          featuredImageUrl: this.model.featuredImageUrl,
+          isVisible: this.model.isVisible,
+          publishedDate: this.model.publishedDate,
+          title: this.model.title,
+          urlHandle: this.model.urlHandle,
+          categories: this.selectedCategories ?? []
+        };
+
+        this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
+          .subscribe({
+            next: (response) => {
+              this.router.navigateByUrl('/admin/blogposts');
+          }
+        })
       }
+    }
+
+  onDelete(): void{
+    if (this.id)
+    {
+      this.deleteBlogPostSusbscription = this.blogPostService.deleteBlogPost(this.id)
+        .subscribe({
+          next: (response) => {
+            this.router.navigateByUrl('/admin/blogposts');
+          }
+        });
+    }
     }
 
     ngOnDestroy(): void {
       this.routeSubscription?.unsubscribe();
+      this.updateBlogPostSubscription?.unsubscribe();
+      this.getBlogPostSusbscription?.unsubscribe();
+      this.deleteBlogPostSusbscription?.unsubscribe();
     }
 
 
